de79a8a09ac3c549471ceb90d907c6a91c9f0234
unknown <you@example.com>
2024-11-18 12:18:22 +0530

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,43 @@
 const cardsData = [
     {
-        image: "assets/image/card-page.png",
-        title: "Talan Torff",
-        subtitle: "Web Developer",
+        image: "assets/image/image 1.png",
+        name: "Gustavo Herwitz",
+        about: "UI/UX Designer",
     },
+
+    {
+        image: "assets/image/image2.png",
+        name: "Phillip Curtis",
+        about: "Graphic Designer",
+    },
+
+    {
+        image: "assets/image/image 3.png",
+        name: "Talan Torff",
+        about: "Web Developer",
+    },
+
+    {
+        image: "assets/image/image 4.png",
+        name: "Abram Vaccaro",
+        about: "App Developer",
+    },
+
 ];
 
 function createCard(cardData) {
     const card = document.createElement('div');
     card.classList.add('col-12', 'col-md-6', 'col-xl-3', 'mb-24');
-    let cardContent = `
+    let cards = `
     <div class="card">
-      <img src="${cardData.image}" class="w-100 mb-4" alt="${cardData.title}">
-      <div class="card-body p-0">
-        <h5 class="card-title poppins-font fw-semibold fs-md  mb-3 text-center">${cardData.title}</h5>
-          <p class="card-subtitle fs-sm poppins-font fw-normal mb-3 text-center">${cardData.subtitle}</p>
+      <img src="${cardData.image}" class="w-100 mb-4" alt="${cardData.name}">
+      <div class="card-body">
+        <h5 class="name mb-3 text-center">${cardData.name}</h5>
+          <p class="about mb-3 text-center">${cardData.about}</p>
         </div>
     </div>
   `;
-    card.innerHTML = cardContent;
+    card.innerHTML = cards;
     return card;
 }
 
